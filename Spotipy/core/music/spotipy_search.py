80cf84d1ce0708511a703aab1dd166fe4596089a
Oren <orengamingil@gmail.com>
2022-02-09 15:29:05 +0200

    Added WIP search class implementation

---
+++ Spotipy/core/music/spotipy_search.py
@@ -0,0 +1,8 @@
+from Spotipy.core.music.spotipy_song_manager import SpotipySongManager
+
+
+class SpotipySearch:
+    def __init__(self, song_manager: SpotipySongManager):
+        self.song_manager = song_manager
+
+    
