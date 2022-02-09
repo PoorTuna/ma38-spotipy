3ba73314a7a082fe1aa7c2738ea8b7d9be26233a
Oren <orengamingil@gmail.com>
2022-02-09 16:12:22 +0200

    Added temp todos. Added WIP get top genre songs function

--- Spotipy/core/music/spotipy_search.py
+++ Spotipy/core/music/spotipy_search.py
@@ -14,9 +14,19 @@ class SpotipySearch:
     def get_artist_albums(self, artist_id):
         return [self.song_manager.albums[album_id] for album_id in self.song_manager.artists[artist_id].album_ids]
 
-    # def get_top_songs(self):
-    #     return sorted([test for test in range(10)],
-    #                   key=operator.itemgetter(-1))[:SearchConstants.most_popular_songs_count]
+    def get_top_songs(self, artist_id):
+        songs_list = []
+        for album_id in self.song_manager.artists[artist_id].album_ids:
+            for song in self.song_manager.albums[album_id][0]:
+                songs_list.append(song)
+
+        return sorted([song for song in songs_list],
+                      key=lambda x: x.popularity)[:SearchConstants.most_popular_songs_count]
 
     def get_album_songs(self, album_id):
         return self.song_manager.albums[album_id][0]
+
+    # TODO : Implement genres (find where is the definition of a song genre??).
+    # TODO : Implement get top genre songs function with constant variables
+    def get_top_genre_songs(self, genre_name):
+        pass
