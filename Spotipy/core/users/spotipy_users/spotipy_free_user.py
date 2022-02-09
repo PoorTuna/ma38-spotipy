4cdecc879752a4b8c94d0b664b65e3008e6c4557
Oren <orengamingil@gmail.com>
2022-02-09 11:40:53 +0200

    Added free user model based on spotipy user model. Added WIP implementation of add playlist function in free user.

--- Spotipy/core/users/spotipy_users/spotipy_free_user.py
+++ Spotipy/core/users/spotipy_users/spotipy_free_user.py
@@ -1,6 +1,11 @@
-from Spotipy.core.users.user import User
+from Spotipy.core.users.spotipy_users.spotipy_user import SpotipyUser
 
 
-class SpotipyFreeUser(User):
-    def __init__(self):
-        super().__init__()
+class SpotipyFreeUser(SpotipyUser):
+    def __init__(self, username, password):
+        super().__init__(username, password)
+
+    def add_playlist(self, playlist_name):
+        if len(self.playlists) <= 5:
+            pass  # raise an exception
+        self.playlists[playlist_name] = []
