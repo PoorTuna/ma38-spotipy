3c9dbebca975faa885a14899e43c5b4521053600
Oren <orengamingil@gmail.com>
2022-02-09 11:33:33 +0200

    Cleaned some code.

--- Spotipy/core/users/spotipy_users/spotipy_user.py
+++ Spotipy/core/users/spotipy_users/spotipy_user.py
@@ -4,6 +4,7 @@ from Spotipy.core.users.user import User
 class SpotipyUser(User):
     def __init__(self, username, password):
         super(SpotipyUser, self).__init__(username, password)
+        self.playlists = {}
 
     def add_playlist(self, playlist_name):
         pass
--- Spotipy/core/users/user.py
+++ Spotipy/core/users/user.py
@@ -5,7 +5,6 @@ class User:
     def __init__(self, username, password):
         self.username = username
         self.password = bcrypt.hashpw(password, bcrypt.gensalt())
-        self.playlists = {}
 
     def validate_password(self, password):
         return bcrypt.checkpw(self.password, password)
