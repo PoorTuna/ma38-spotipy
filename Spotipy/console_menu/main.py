1db339e9e8fd39179d8e2ba85ce339dfa58700f0
Oren <orengamingil@gmail.com>
2022-02-10 16:19:56 +0200

    Implemented a simple register, login and logout menu functions

--- Spotipy/console_menu/main.py
+++ Spotipy/console_menu/main.py
@@ -4,21 +4,26 @@ from consolemenu.items import *
 from Spotipy.spotipy import Spotipy
 
 
-def login_input():
-    username = input("Enter a username : ")
-    password = input("Enter a password : ")
-
-    return [username, password]
-
-
 def spotipy_console():
     # Main Menu:
     my_spotipy = Spotipy()
+
+    # Related menu functions
+    def login_input():
+        username = input("Enter a username : ")
+        password = input("Enter a password : ")
+
+        my_spotipy.user_manager.login(username, password)
+
+    def signup_input():
+        username = input("Enter a username : ")
+        password = input("Enter a password : ")
+
+        my_spotipy.user_manager.signup(username, password)
+
     menu = ConsoleMenu("Spotipy", "Main Menu")
 
-    # menu_item = MenuItem("menu item test")
     func_item = FunctionItem("Running a command", input, ["Enter an input"])
-    # command_item = CommandItem("Run a console command", "color a")
 
     selection_menu = SelectionMenu(["1 M", "2 M ", "3 M"])
 
@@ -26,10 +31,17 @@ def spotipy_console():
 
     login_menu = FunctionItem("Sign In", login_input)
 
-    register_menu = SubmenuItem("Sign Up", selection_menu, menu)
+    # Register Menu:
+    register_menu = FunctionItem("Sign Up", signup_input)
 
+    # Logout Menu:
+
+    logout_menu = FunctionItem("Log Out", my_spotipy.user_manager.sign_out)
+
+    # Menu init :
     menu.append_item(login_menu)
     menu.append_item(register_menu)
+    menu.append_item(logout_menu)
 
     menu.show()
 
