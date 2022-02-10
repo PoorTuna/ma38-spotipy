9533cff027b77294998ffcebe5b21b7344b317bf
Oren <orengamingil@gmail.com>
2022-02-10 08:56:37 +0200

    Implemented a better is authenticated function. Implemented a sign out function

--- Spotipy/core/users/user_manager.py
+++ Spotipy/core/users/user_manager.py
@@ -1,13 +1,22 @@
+from Spotipy.core.users.spotipy_users.spotipy_user import SpotipyUser
+
+
 class UserManager:
     def __init__(self):
         self.users = {}
 
-    def signup(self, username, password):
+    def signup(self, username: str, password: str):
         pass
 
-    def login(self, username, password):
+    def login(self, username: str, password: str):
         pass
+
+    def sign_out(self):
+        self.CurrentUser.curr_user = None
 
     class CurrentUser:
         curr_user = None
-        is_authenticated = False
+
+        @staticmethod
+        def is_authenticated(curr_user: SpotipyUser = curr_user):
+            return curr_user if curr_user is not None else None
